5b1e556b90e25eea0412c50abd09613baa52f6f6
wda <andreas.weidinger@integrating-architecture.de>
2024-10-18 17:53:57 +0200

    initial update

--- src/test/java/org/isa/ipc/JamnServerTest.java
+++ src/test/java/org/isa/ipc/JamnServerTest.java
@@ -1,12 +1,10 @@
-package org.isa.snip.ipc;
-
-import org.junit.jupiter.api.Test;
+package org.isa.ipc;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.isa.ipc.JamnServer;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test.

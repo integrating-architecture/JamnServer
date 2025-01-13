105f2d33df93a62ba8cdc68c58bb9c9fac94a2f5
wda <andreas.weidinger@integrating-architecture.de>
2025-01-13 14:44:21 +0100

    snapshot

---
+++ org.isa.jps.JamnPersonalServerApp/http/README.md
@@ -0,0 +1,3 @@
+# Jamn Web Workbench 
+
+An example of a slim, independent web gui e.g. for individual tools.
--- org.isa.jps.JamnPersonalServerApp/scripts/js-info.js
+++
@@ -1,7 +0,0 @@
-
-info = [
-	["Graal.versionECMAScript" , Graal.versionECMAScript],
-	["Graal.versionGraalVM" , Graal.versionGraalVM]
-];
-
-val = [...info.values()].map(e => e.join(' = ')).join('\n');
